740a909f36e2c01694a40351a0b1f04e0efe08e5
Iaannn06 <alfianazad05@gmail.com>
2024-06-14 10:21:13 +0700

    melakukan postorder transversal dari tree

--- a/a.cpp
+++ a/a.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node {
 public:
 	string info;
-	Node *leftchild;
+	Node* leftchild;
 	Node* rightchild;
 
 	Node(string i, Node* l, Node* r) {
@@ -33,7 +33,7 @@ public:
 
 		Node* parent = NULL;
 		Node* currentNode = NULL;
-		search (element, parent, currentNode);
+		search(element, parent, currentNode);
 		if (parent == NULL)
 		{
 			ROOT = newNode;
@@ -48,6 +48,59 @@ public:
 			parent->rightchild = newNode;
 
 		}
-
 	}
+	void search(string element, Node*& parent, Node*& currentNode) {
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+	void inorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+	void postorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+
 };
